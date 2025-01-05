fd7d8bfec12b0a28d18c7915a88fe5cfc026d0c9
HussainKhan43 <hussainkhan5march@gmail.com>
2025-01-05 15:06:50 +0530

    updt

--- Backend/index.js
+++ Backend/index.js
@@ -14,16 +14,16 @@ const DB_URI = process.env.MONGODB_URI;
 // middlewares
 app.use(express.json());
 app.use(cookieParser());
-// app.use(
-//   cors({
-//     origin: process.env.FRONTEND_URL,
-//     credentials: true,
-//     methods: "GET,POST,PUT,DELETE",
-//     allowedHeaders: ["Content-Type", "Authorization"], // Add other headers you want to allow here.
-//   })
-// );
 
-app.use(cors())
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+    methods: "GET,POST,PUT,DELETE",
+    allowedHeaders: ["Content-Type", "Authorization"], // Add other headers you want to allow here.
+  })
+);
+
 
 // Database connection code
 try {
